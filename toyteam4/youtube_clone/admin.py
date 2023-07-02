e7ad9195daa0f94ef4fee9c2e569988dc93bd66d
bamcasa <combeesang@gmail.com>
2023-07-02 23:14:18 +0900

    FIX : admin.py
    model register

--- toyteam4/youtube_clone/admin.py
+++ toyteam4/youtube_clone/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Video, Recommended_Videos, Comment, Reply
 
 # Register your models here.
+
+admin.site.register(Video)
+admin.site.register(Recommended_Videos)
+admin.site.register(Comment)
+admin.site.register(Reply)
