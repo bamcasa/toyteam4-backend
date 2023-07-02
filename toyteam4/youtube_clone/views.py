11129620026c1f99545418ae3430e18a7afcb29e
bamcasa <combeesang@gmail.com>
2023-07-02 23:13:57 +0900

    FIX : views.py

--- toyteam4/youtube_clone/views.py
+++ toyteam4/youtube_clone/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Video
 # Create your views here.
 
 
 def test(request):
     return HttpResponse("It is TEST page")
+
+
+def test_video(request):
+    videos = Video.objects.all()
+    return render(request, 'Videos.html', {'videos': videos})
