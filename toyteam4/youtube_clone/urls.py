ca91789e91a726f58e27ed214d6fa18419f03a34
bamcasa <combeesang@gmail.com>
2023-07-02 23:13:43 +0900

    FIX : urls.py

--- toyteam4/youtube_clone/urls.py
+++ toyteam4/youtube_clone/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import test
+from .views import test, test_video
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('test/', test, name='test'),
+    path('Videos/', test_video, name='Videos')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
