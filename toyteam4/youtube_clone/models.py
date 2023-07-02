81d56b3f69a4730a201c60c5ed5d970d6844dbe7
bamcasa <combeesang@gmail.com>
2023-07-02 23:13:29 +0900

    FIX : models.py
    Video, Recommended_Videos, Comment, Reply 4개의 모델 생성

--- toyteam4/youtube_clone/models.py
+++ toyteam4/youtube_clone/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Video(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    # video_file = models.FileField(upload_to='videos/')
+    thumbnail = models.ImageField(upload_to='thumbnails/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Recommended_Videos(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    comment = models.TextField()
+
+
+class Reply(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    reply = models.TextField()
