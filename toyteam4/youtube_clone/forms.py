06f9ace710b2be4daca48ff50af1d6a486942862
bamcasa <combeesang@gmail.com>
2023-07-06 00:03:27 +0900

    ADD: Image 업로드

---
+++ toyteam4/youtube_clone/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Video
+
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Video
+        fields = ['title', 'description', 'video_file', 'thumbnail']
