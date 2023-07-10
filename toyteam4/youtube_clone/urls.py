8be323cbf8e5ce8425ef2b510d2aacf09cb75f34
bamcasa <combeesang@gmail.com>
2023-07-10 18:26:42 +0900

    Feat: 각 모델별 엔드포인트 지정
    recommeded_videos
    body로 user_id를 받아 여러개의 비디오를 반환

--- toyteam4/youtube_clone/urls.py
+++ toyteam4/youtube_clone/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import test, test_video, upload_video, view_video
-from .views import VideoViewSet
+from .views import test, test_video
+from .views import *
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -10,12 +10,18 @@ from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'videos', VideoViewSet)
+router.register(r'users', UserViewSet)
+router.register(r'comments', CommentViewSet)
+router.register(r'replies', ReplyViewSet)
+router.register(r'rdvideos', Recommended_VideosViewSet,
+                basename='recommended-videos')
+
 
 urlpatterns = [
     path('test/', test, name='test'),
     path('Videos/', test_video, name='Videos'),
-    path('upload/', upload_video, name='upload'),
-    path('view_video/<str:title>/', view_video, name='view_video'),
+    # path('upload/', upload_video, name='upload'),
+    # path('view_video/<str:title>/', view_video, name='view_video'),
     path('api/', include(router.urls)),
 ]
 
